edac0badaa5abdbd7e1fbf7e3a593cb872c9a94c
jiangshuguang <jiangshuguangzz@163.com>
2013-06-25 20:14:45 +0800

    修改page

---
+++ config.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <link type="text/css" rel="stylesheet" href="./static/css/common.css"/>
+    <link type="text/css" rel="stylesheet" href="./static/css/index.css"/>
+    <script type="text/javascript" src="./static/js/jquery-1.8.2.js"></script>
+    <script type="text/javascript" src="./static/js/index.js"></script>
+</head>
+<body>
+<div class="header">
+    <div class="logo"></div>
+    <div class="nav">
+        <ul>
+            <a href="index.php"><li>全部</li></a>
+            <a href="filter.php"><li>过滤</li></a>
+            <a href="config.php"><li style="background-color: #ffffff;color:#000000;">配置</li></a>
+        </ul>
+    </div>
+</div>
+
+<div class="body">
+    <table align="center" cellpadding="8" width="100%"  border="1" id="mobileList" style=" border-collapse:collapse;table-layout: fixed;word-break: break-all;text-align:center;border-color: #dfdfdf">
+        <tr bgcolor="#ae9c34" style="color:#ffffff;">
+            <td width="5%">ip</td>
+            <td width="10%">端口</td>
+            <td width="10%">系统版本</td>
+            <td width="10%">mobile别名</td>
+            <td width="65%">浏览器操作</td>
+        </tr>
+    </table>
+</div>
+
+</body>
+</html>
